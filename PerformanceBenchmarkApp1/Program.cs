3ed78010c800f70cc78eb760423e0ec874beb185
Bayram Akbuz <bayram.akbuz@haibooks.com>
2020-01-30 10:14:17 +0300

    Benchmark

---
+++ PerformanceBenchmarkApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PerformanceBenchmarkApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
